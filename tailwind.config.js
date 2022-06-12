b97c5335e33315f0d8e2b89d0a2c6f8aec0d7186
dennykd <kadekdenny61@gmail.com>
2022-06-12 22:07:15 +0800

    new layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: "12px",
+      },
+      colors: {
+        primary: "#f87171",
+        secondary: "#64748b",
+        dark: "#111827",
+      },
+      fontFamily: {
+        montserrat: ["montserrat", "sans-serif"],
+        lato: ["lato", "sans-serif"],
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
   },
   plugins: [],
 };
